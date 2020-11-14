28152fb775db3e0c5b974619bff1c0de42755f28
syrix78 <lucas.hornung@hotmail.com>
2020-11-14 01:03:41 -0500

    Combine everything into main.py. Added Q3 and Q4 results

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from nltk.tag.brill import Pos, Word
 train_sentences = treebank.tagged_sents()[:3000]
 test_sentences = treebank.tagged_sents()[3000:]
 
+
 """
 Question 3: Redefine this function so that it considers the context
 """
-def feature_func(tokens, idx):
+def feature_func(tokens, idx, window_size=1):
     """
             Extract basic features about this word including
                  - Current Word
@@ -32,37 +33,82 @@ def feature_func(tokens, idx):
             """
 
     pattern = re.compile(r"\d")
-    token = tokens[idx]
+
+    actual_word_idx = idx
+
+    idx -= window_size # Pour se rendre au debut de la fenetre
 
     feature_list = []
 
-    if not token:
-        return feature_list
+    indicator = ""
 
-    # Capitalization
-    if token[0].isupper():
-        feature_list.append("CAPITALIZATION")
+    before = True
+    after = False
+    features = False
+    using_word = True
 
-    # Number
-    if re.search(pattern, token) is not None:
-        feature_list.append("HAS_NUM")
+    for i in range(window_size*2 + 1):
 
-    # Punctuation
-    punc_cat = set(["Pc", "Pd", "Ps", "Pe", "Pi", "Pf", "Po"])
-    if all(unicodedata.category(x) in punc_cat for x in token):
-        feature_list.append("PUNCTUATION")
+        if(idx < actual_word_idx):
+            pos = actual_word_idx - idx
+            indicator = "PRE_" + str(pos) + "_"
+        elif(idx == actual_word_idx):
+            indicator = ""
+        else:
+            pos = idx - actual_word_idx
+            indicator = "POST_" + str(pos) + "_"
 
-    # Suffix up to length 3
-    if len(token) > 1:
-        feature_list.append("SUF_" + token[-1:])
-    if len(token) > 2:
-        feature_list.append("SUF_" + token[-2:])
-    if len(token) > 3:
-        feature_list.append("SUF_" + token[-3:])
+        if(idx < 0):
+            idx += 1
 
-    feature_list.append("WORD_" + token)
+        elif(idx >= len(tokens)):
+            break
+
+        elif(idx < actual_word_idx and after):
+            idx += 1
+
+        elif(idx > actual_word_idx and before):
+            break
+
+        else:
+
+            token = tokens[idx]
+
+            if not token:
+                return feature_list
+
+            if (idx == actual_word_idx or features):
+                # Capitalization
+                if token[0].isupper():
+                    feature_list.append(indicator + "CAPITALIZATION")
+
+                # Number
+                if re.search(pattern, token) is not None:
+                    feature_list.append(indicator + "HAS_NUM")
+
+                # Punctuation
+                punc_cat = set(["Pc", "Pd", "Ps", "Pe", "Pi", "Pf", "Po"])
+                if all(unicodedata.category(x) in punc_cat for x in token):
+                    feature_list.append(indicator + "PUNCTUATION")
+
+                # Suffix up to length 3
+                if len(token) > 1:
+                    feature_list.append(indicator + "SUF_" + token[-1:])
+                if len(token) > 2:
+                    feature_list.append(indicator + "SUF_" + token[-2:])
+                if len(token) > 3:
+                    feature_list.append(indicator + "SUF_" + token[-3:])
+
+            if (idx == actual_word_idx):
+                feature_list.append("WORD_" + token)
+            elif (using_word):
+                feature_list.append(indicator + "WORD_" + token)
+                feature_list.append(indicator + "WORD_" + token + "/" + tokens[actual_word_idx])
+
+            idx += 1
 
     return feature_list
+
 
 def question3():
     tagger = CRFTagger(feature_func=feature_func)
@@ -73,10 +119,10 @@ def question3():
     return
 
 """
-Question 4: Train using BrillTagger
+Used to compare meta-params
 """
-def question4():
-    #Taken from http://www.nltk.org/book/ch05.html
+def meta_comparison(nb_iterations):
+    # Taken from http://www.nltk.org/book/ch05.html
     patterns = [
         (r'.*ing$', 'VBG'),  # gerunds
         (r'.*ed$', 'VBD'),  # simple past
@@ -86,29 +132,78 @@ def question4():
         (r'.*s$', 'NNS'),  # plural nouns
         (r'^-?[0-9]+(\.[0-9]+)?$', 'CD'),  # cardinal numbers
         (r'.*', 'NN')  # nouns (default)
-        ]
-
-    #init_tagger = CRFTagger(feature_func=feature_func)
-    #init_tagger.train(train_sentences, 'model.crf.tagger')
-
+    ]
     init_tagger = RegexpTagger(patterns)
 
-    nb_iterations = 31
-    currentTagger = None
-    current_evaluation = 0.0
     evaluations = []
 
     for i in range(1, nb_iterations):
         # https://www.nltk.org/api/nltk.tag.html#module-nltk.tag.brill_trainer
         Template._cleartemplates()
-        templates = [Template(Pos([-1])), Template(Pos([-1]), Word([0]))]
+        template = [Template(Pos([-1])), Template(Pos([-1]), Word([0]))]
 
-        tt = BrillTaggerTrainer(init_tagger, templates, trace=3)
+        tt = BrillTaggerTrainer(init_tagger, template, trace=3)
         currentTagger = tt.train(train_sentences, max_rules=i*50)
         current_evaluation = currentTagger.evaluate(test_sentences)
         evaluations.append(current_evaluation)
 
-    """
+    return evaluations
+
+
+"""
+Used to compare templates
+"""
+def template_comparison(nb_iterations):
+    # Taken from http://www.nltk.org/book/ch05.html
+    patterns = [
+        (r'.*ing$', 'VBG'),  # gerunds
+        (r'.*ed$', 'VBD'),  # simple past
+        (r'.*es$', 'VBZ'),  # 3rd singular present
+        (r'.*ould$', 'MD'),  # modals
+        (r'.*\'s$', 'NN$'),  # possessive nouns
+        (r'.*s$', 'NNS'),  # plural nouns
+        (r'^-?[0-9]+(\.[0-9]+)?$', 'CD'),  # cardinal numbers
+        (r'.*', 'NN')  # nouns (default)
+    ]
+    init_tagger = RegexpTagger(patterns)
+    templates = [nltk.tag.brill.nltkdemo18(), nltk.tag.brill.nltkdemo18plus(), nltk.tag.brill.fntbl37(), nltk.tag.brill.brill24()]
+    evaluations = []
+
+    for t in templates:
+        # https://www.nltk.org/api/nltk.tag.html#module-nltk.tag.brill_trainer
+        Template._cleartemplates()
+
+
+        tt = BrillTaggerTrainer(init_tagger, t, trace=3)
+        currentTagger = tt.train(train_sentences)
+        current_evaluation = currentTagger.evaluate(test_sentences)
+        evaluations.append(current_evaluation)
+
+    return evaluations
+
+"""
+Used to test Brill(...Brill(Regexp)) and Brill(...Brill(CRF))
+"""
+def Brill_recursion(nb_iterations):
+    # Taken from http://www.nltk.org/book/ch05.html
+    patterns = [
+        (r'.*ing$', 'VBG'),  # gerunds
+        (r'.*ed$', 'VBD'),  # simple past
+        (r'.*es$', 'VBZ'),  # 3rd singular present
+        (r'.*ould$', 'MD'),  # modals
+        (r'.*\'s$', 'NN$'),  # possessive nouns
+        (r'.*s$', 'NNS'),  # plural nouns
+        (r'^-?[0-9]+(\.[0-9]+)?$', 'CD'),  # cardinal numbers
+        (r'.*', 'NN')  # nouns (default)
+    ]
+
+    # init_tagger = CRFTagger(feature_func=feature_func)
+    # init_tagger.train(train_sentences, 'model.crf.tagger')
+    init_tagger = RegexpTagger(patterns)
+    currentTagger = None
+    current_evaluation = 0.0
+    evaluations = []
+
     for i in range(nb_iterations):
         #Not sure if we need to use BrillTagger or BrillTaggerTrainer??
         #https://www.nltk.org/api/nltk.tag.html#module-nltk.tag.brill_trainer
@@ -127,17 +222,35 @@ def question4():
             current_evaluation = tagger.evaluate(test_sentences)
             evaluations.append(current_evaluation)
             currentTagger = tagger
-    """
+
     print(current_evaluation)
     return evaluations
+
+"""
+Question 4: Train using BrillTagger
+"""
+def question4():
+
+    nb_iterations = 31
+
+
+    template_evals = template_comparison(nb_iterations)
+    #meta_evals = meta_comparison(nb_iterations)
+    #recursion_evals = Brill_recursion(nb_iterations)
+
+    evaluations = template_evals
+
+    return evaluations
+
 
 if __name__ == "__main__":
     #question3()
     question4_evals = question4()
 
-    with open('question4_meta_max_rules_one.csv', mode='w+') as file:
+    #Allows us to print results into a csv
+    with open('question.csv', mode='w+') as file:
         employee_writer = csv.writer(file, delimiter=',')
 
-        employee_writer.writerow(list(range(50, len(question4_evals)*50 + 1, 50)))
+        employee_writer.writerow(list(range(len(question4_evals))))
         employee_writer.writerow(question4_evals)
 
